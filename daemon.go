65df812659afcfd93b6d36058e1fd25de9501495
bchisham <bchisham@carregistration.com>
2024-12-20 15:40:41 -0800

    # Daemon Control
    Adds support for starting the daemon process.

---
+++ daemon.go
@@ -0,0 +1,40 @@
+package clamd
+
+import (
+	"os"
+	"os/exec"
+)
+
+type daemonConfig struct {
+	configFile os.File
+}
+
+type DaemonOption func(*daemonConfig)
+
+func WithConfigFile(configFile os.File) DaemonOption {
+	return func(c *daemonConfig) {
+		c.configFile = configFile
+	}
+}
+
+type Server struct {
+	daemonConfig
+}
+
+func NewDaemon(opts ...DaemonOption) *Server {
+	defaults := &daemonConfig{}
+	for _, opt := range opts {
+		opt(defaults)
+	}
+	return &Server{
+		daemonConfig: *defaults,
+	}
+}
+
+func (s *Server) Start() error {
+	runner := exec.Command("clamd", "--config-file", s.configFile.Name())
+	runner.Stdin = nil
+	runner.Stdout = os.Stdout
+	runner.Stderr = os.Stderr
+	return runner.Run()
+}
